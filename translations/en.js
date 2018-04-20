676e1208db2f5d9ce9b3670bebabb78256faa878
Martin Stein <mst206ster@googlemail.com>
2018-04-20 08:31:40 +0200

    English Translation
    Not checked if working :D

---
+++ translations/en.js
@@ -0,0 +1,53 @@
+export default ['en', {
+  od: {
+    core: {
+
+    },
+    auth: {
+      login: 'Login',
+      signup: 'Create Account',
+      logout: 'Logout',
+      noaccount: 'No Account? Click Here',
+      resetpassword: 'Forgot Password? Click Here',
+      button: {
+        login: 'Login',
+        signup: 'create',
+      },
+    },
+    header: {
+      dashboards: {
+        header: 'Dashboards',
+        manage: 'Manage Dashboards',
+        create: 'Create new Dashboard',
+        create_prompt: 'Please enter a name for the new Dashboard!',
+        delete: 'Delete currently selected Dashboard',
+        delete_prompt: 'Are you sure you want to delete the current Dashboard?',
+      },
+      widgets: {
+        header: 'Widgets',
+        presets: 'Add Widget from template',
+        eud: 'Create new analytic Widget',
+      },
+    },
+    dashboard: {
+      empty: 'No Widgets found :(',
+      widgets: {
+        add: 'Add Widget from template',
+        rename: 'Provide a name for the widget:',
+        remove: 'Are you sure you want to delete the widget?',
+        configurate: 'Settings',
+        noconfig: 'Please configure the widget',
+        save: 'Save',
+        abort: 'Cancel',
+      },
+    },
+    presets: {
+      add: 'Hinzufügen',
+    },
+    select: {
+      start: 'Startdatum:',
+      end: 'Enddatum:',
+      since: 'Seit:',
+    },
+  },
+}];
