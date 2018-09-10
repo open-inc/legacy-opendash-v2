e9ef55a911344f993d31dc911b5ace2931bb2a92
Sebastian Zilles <dev@zilles.io>
2018-09-10 12:05:02 +0200

    Update eud-widget

--- plugins/eud/eud-widget/widget/widget.controller.js
+++ plugins/eud/eud-widget/widget/widget.controller.js
@@ -1,22 +1,37 @@
-export default ['opendash/services/eud', 'opendash/services/event', ($eud,$event) => {
+export default ['opendash/services/eud', 'opendash/services/event', '$q', ($eud, $event, $q) => {
     return {
-        $onInit() {
-            const selected = this.config.selected;
-            const settings = this.config.settings;
-            $eud.getItems()
-                .then(items => $eud.getHistory(items, selected, settings))
-                .then(history => $eud.getChart(history, settings))
-                .then(graph => {
+        async $onInit() {
+            try {
+                const selected = this.config.selected;
+                const settings = this.config.settings;
 
-                    this.graph = graph;
-                    this.loading = false;
+                let items = await $eud.getItems();
+                let history = await $eud.getHistory(items, selected, settings);
+                let graph = await $eud.getChart(history, settings);
 
-                    $event.on(['od-widgets-resize', 'od-widgets-changed'], () => {
-                        this.graph.getChartObj().reflow();
+                this.graph = graph;
+                this.loading = false;
 
+                if (this.widget && this.widget.onRequest) {
+                    this.widget.onRequest('sharing:export', async () => {
+                        return history.map(i => {
+                            return {
+                                id: i.item.id,
+                                name: i.item.name,
+                                history: i.history,
+                            };
+                        });
                     });
+                }
 
+                $event.on(['od-widgets-resize', 'od-widgets-changed'], () => {
+                    this.graph.getChartObj().reflow();
                 });
+
+                await $q.resolve();
+            } catch (error) {
+                console.error('eud-widget error:', error);
+            }
         },
     };
 }];
