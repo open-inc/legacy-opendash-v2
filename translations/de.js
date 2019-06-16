37d2cfb38f72f01ad245f8d620e1699613335a13
Nico Castelli <nico.castelli@uni-siegen.de>
2019-06-16 18:35:50 +0200

    Greis Translation

--- translations/de.js
+++ translations/de.js
@@ -1,6 +1,11 @@
 export default [
   "de",
   {
+    ow: {
+      reporting: {
+        createScheduled: "Neuen regelmäßigen Bericht erstellen"
+      }
+    },
     od: {
       core: {},
       auth: {
